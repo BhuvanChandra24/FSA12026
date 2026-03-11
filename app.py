43af26b489fd203e791bb2bb940dc03f8f1ad409
Bhuvan Chandra <bhuvanchandra24@gmail.com>
2026-03-11 14:38:44 +0530

    changed app.py file

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ model.fit(X, y)
 
 prediction = model.predict([[6]])    
 print(f"Predicted salary for 6 years of experience: {prediction[0]}")   
+
+print("NGIT is deployed successfully the project")
